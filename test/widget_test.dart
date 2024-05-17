b491dbd2cdbf3f339adf251b95c90b57b2835c3f
AliAkhmat <aliakhmat31@gmail.com>
2024-05-17 17:24:12 +0300

    add architecture and home view

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:baykot/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Baykot());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
